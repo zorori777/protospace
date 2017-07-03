d72b9c4f4a1c63cab899e3a05b7eb78459b6c93f
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-03 16:33:10 +0900

    Making_session_function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :authenticate_user!
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :member, :profile, :work, :image])
+  end
 end
+
