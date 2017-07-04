2269d7cf8fb504ecefda4bb7c998fa35a5b6eb69
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-04 21:59:37 +0900

    Adaption_user_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, :profile, presence: true
+  mount_uploader :image, ImageUploader
 end
