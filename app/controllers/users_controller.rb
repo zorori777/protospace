2dfabeb243a4f7d26c0241a054d8c786f04167a1
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-04 15:19:04 +0900

    Making_edit_and_update_functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,40 @@
 class UsersController < ApplicationController
 
-  before_action :setting_user, only: %i[edit show]
+  before_action :authenticate_user!, only: %i[edit update]
+
+  before_action :setting_user,       only: %i[edit update show]
 
   def index
-  end
-
-  def edit
   end
 
   def show
   end
 
+  def edit
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+       redirect_to user_path, notice: "Updat is success!!"
+    else
+      flash.now[:alert] = "Update is false!"
+      render :edit
+    end
+  end
+
 private
+
   def setting_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :profile,
+                                 :image,
+                                 :member,
+                                 :work)
   end
 end
