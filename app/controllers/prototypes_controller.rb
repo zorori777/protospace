e5bdff92726f01feb3c1939a3cdcada1a27b1a4d
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-05 21:07:42 +0900

    Making_crate_function

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,5 +2,21 @@ class PrototypesController < ApplicationController
 
   def new
     @prototype = Prototype.new
+    @prototype.capture_images.build
+  end
+
+  def create
+    @prototype = current_user.prototypes.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: "Post is success"
+    else
+      redirect_to new_prototype_path, alert: "Post is false"
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept,capture_images_attributes: [:content, :role])
   end
 end
