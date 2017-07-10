9cf13bfa3470db1a320c0d9c93135ac39c4f5af0
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-10 17:01:14 +0900

    Modify_reviews

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,7 +23,7 @@ class PrototypesController < ApplicationController
 
   def show
     @comment = Comment.new
-    @comments = @prototype.comments
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
