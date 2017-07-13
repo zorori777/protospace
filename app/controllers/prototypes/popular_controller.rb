a9322500035f2e0bad0571317573ff2312c9f30e
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-13 10:31:32 +0900

    Implement_pagination-function

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,6 +1,6 @@
 class Prototypes::PopularController < PrototypesController
 
   def index
-    @prototype = Prototype.includes([:user, :tags]).order(likes_count: :DESC).limit(5)
+    @prototype = Prototype.includes([:user, :tags]).order(likes_count: :DESC).page(params[:page]).per(5)
   end
 end
