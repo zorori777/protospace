51f4dba1006a2953973145388dccae96d72ad05d
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-25 15:39:11 +0900

    Add_created_at_column

--- spec/factories/prototype.rb
+++ spec/factories/prototype.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     title                  { Faker::Pokemon.name }
     catch_copy             { Faker::Cat.name }
     concept                { Faker::Lorem.sentence }
+    created_at { Faker::Time.between(2.days.ago, Date.today, :all) }
     user
   end
 end
