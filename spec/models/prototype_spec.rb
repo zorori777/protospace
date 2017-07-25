0523facd4314c687ee3132cbe4d9cc5ee1a54eb7
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-25 15:38:37 +0900

    Testing_validation

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -2,11 +2,33 @@ require 'rails_helper'
 
 describe Prototype do
   describe '#create' do
-    context 'validation' do
-      it "is invalid without title" do
-        prototype = build(:prototype, title:  "")
-        prototype.valid?
-        expect(prototype.errors[:title]).to include("can't be blank")
+    let(:prototype) { build(:prototype) }
+    describe 'validation' do
+      let(:prototype_without_title) { build(:prototype, title: '') }
+      let(:prototype_without_catch_copy) { build(:prototype, catch_copy: '') }
+      let(:prototype_without_concept) {build(:prototype, concept: '') }
+
+      context 'valid date' do
+        it 'is valid prototype' do
+          expect(prototype).to be_valid
+        end
+      end
+
+      context 'invalid date' do
+        it "is invalid without a title" do
+          prototype_without_title.valid?
+          expect(prototype_without_title.errors[:title]).to include("can't be blank")
+        end
+
+        it "is invalid without a catch_copy" do
+          prototype_without_catch_copy.valid?
+          expect(prototype_without_catch_copy.errors[:catch_copy]).to include("can't be blank")
+        end
+
+        it "is invalid without a concept" do
+          prototype_without_concept.valid?
+          expect(prototype_without_concept.errors[:concept]).to include("can't be blank")
+        end
       end
     end
   end
