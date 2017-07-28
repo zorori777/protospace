412eacb4387e5d571f6b6a3cff132f7b1976c01c
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-28 14:17:57 +0900

    日本語化に伴って全体を編集

--- spec/models/capture_image_spec.rb
+++ spec/models/capture_image_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe CaptureImage do
   describe '#create' do
     describe 'validation' do
-      context 'valid date' do
+      context '有効なデーターの時' do
         let(:image){ create(:capture_image, :main) }
-        it "is valid CaptureImage" do
+        it "CaptureImageが有効な時" do
           image.valid?
           expect(image).to be_valid
         end
       end
-      context 'invalid date' do
+      context '無効なデーターの時' do
         let(:image_without_role) { build(:capture_image, role: '') }
-        it "is invalid without a role" do
+        it "roleが空欄のとき" do
           image_without_role.valid?
           expect(image_without_role.errors[:role]).to include("を入力してください。")
         end
@@ -22,14 +22,14 @@ describe CaptureImage do
     describe 'association' do
       let!(:prototype) { create(:prototype) }
       let(:image){ create(:capture_image, :main, prototype: prototype) }
-      it "is associated with prototype" do
+      it "prototypeとの関連性" do
         expect(image.prototype).to eq prototype
       end
     end
 
     describe 'valid_image_format' do
       let!(:content_invalid_format) { build(:capture_image, :main, :invalid_format) }
-      it "has the invalid content format" do
+      it "画像の有効なformat" do
         content_invalid_format.valid?
         expect(content_invalid_format.errors[:content]).to include("rmagickがファイルを処理できませんでした。画像を確認してください。")
       end
