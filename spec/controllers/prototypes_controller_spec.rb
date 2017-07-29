a08e95679e9ea79a9287e334a7fe78113cfba9af
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-29 15:48:05 +0900

    Refoctoring_edit_and_update

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -13,59 +13,62 @@ describe PrototypesController do
     end
 
     describe "GET #edit" do
-      it "assigns the requested prototype to @prototype" do
+      before do
         get :edit, id: prototype
+      end
+
+      it "assigns the requested prototype to @prototype" do
         expect(assigns(:prototype)).to eq prototype
       end
 
       it "renders the :edit template" do
-        get :edit, id: prototype
         expect(response).to render_template :edit
       end
 
       it "assigns main_image to @main_image" do
-        get :edit, id: prototype
         expect(assigns(:main_image)).to eq prototype.capture_images.main.first
       end
 
       it "assigns sub_image to @sub_image" do
-        get :edit, id: prototype
         expect(assigns(:sub_image)).to eq prototype.capture_images.sub.first
       end
     end
 
     describe "PATCH #update" do
+
       context "When save is success" do
-        it "assigns the requested prototype to @prototype" do
+        before do
           patch :update, update_params
+        end
+
+        it "assigns the requested prototype to @prototype" do
           expect(assigns(:prototype)).to eq prototype
         end
 
         it "updates attributes of prototype" do
-          patch :update, update_params
           prototype.reload
           expect(prototype.title).to eq "takumi"
         end
 
-        it "redirect to root_path " do
-          patch :update, update_params
+        it "redirects to root_path " do
           expect(response).to redirect_to root_path
         end
 
         it "flashes a notice  save message" do
-          patch :update, update_params
           expect(flash[:notice]).to include('Update is success')
         end
       end
 
       context "When save is false" do
-        it "redirects edit page when save is false" do
+        before do
           patch :update, update_blank_title_params
+        end
+
+        it "redirects edit page when save is false" do
           expect(response).to redirect_to prototype_path
         end
 
         it "flashes an alert unsave message" do
-          patch :update, update_blank_title_params
           expect(flash[:alert]).to include("Titleを入力してください。")
         end
       end
