29cadd459735ecd38eef6ddec5da747101f761a9
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-07-31 01:45:42 +0900

    Modify_login_user

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,6 +1,6 @@
 module ControllerMacros
-  def login_user(user)
+  def login_user
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    sign_in user
+    sign_in create(:user)
   end
 end
